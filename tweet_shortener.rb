209e5d4ce52dfd2a66763e0eebbe950033b767ec
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 16:52:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,5 +2,22 @@
 
 def dictionary 
 dictionary = {
-  
+    :hello => "hi",
+    :to => "2",
+    :two => "2",
+    :too => "2",
+    :for => "4",
+    :four => "4",
+    :be => "b",
+    :you => "u",
+    :at => "@",
+    :and => "&"
 }
+
+def word_substituter(tweets)
+
+end  
+  
+  
+  
+  
