67f07165e13f0a9f0a0e59bdb7a5d0e15b652ae7
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 17:44:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,6 +33,9 @@ array.each do |tweet| puts word_substituter(tweet)
 end
 end
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+end
   
   
